b4af5624e7327da0295a98f729456e41d2eccf5e
Lalit Shahi <shahilalit002@gmail.com>
2023-09-13 20:54:11 +0530

    user button provided by clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-3xl text-green-600 font-bold">Hello World!</div>;
+  return (
+    <div className="text-3xl text-green-600 font-bold">
+      Hello World!
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
